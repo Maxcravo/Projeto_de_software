959f72407be1644e68dbf048906f175a378c4538
sarah <sarahborretti@gmail.com>
2023-07-08 12:34:01 -0300

    Página de visualizar ficha colocada com react
    A página foi adaptada pro react a partir da branch da carol. Arquivos antigos agora estão na pasta OLD.

---
+++ vet-dor/src/app/page.js
@@ -0,0 +1,23 @@
+import Image from 'next/image'
+import styles from './page.module.css'
+import Header from './components/Header/header.js'
+import Footer from './components/Footer/Footer'
+import Cadastro_Form from './components/CadastroForm/Cadastro_Form'
+import AniProfile from './components/AniProfile/AniProfile'
+import Login_Form from './components/LoginForm/Login_Form'
+
+export default function Home() {
+  return (
+    <main >
+      {/* <Header/> */}
+
+      {/*<Cadastro_Form/>*/}
+
+      <Login_Form />
+
+      {/* <AniProfile/> */}
+      
+      {/* <Footer/> */}
+    </main>
+  )
+}
